0f18c6d4912003c845695da5484ae5afb58a3a4d
StefanUrilski <urilskistefan@gmail.com>
2019-08-20 15:43:19 +0300

    Initial commit. Unit tests for Supplier Service added.

--- src/test/java/org/softuni/cardealer/service/SupplierServiceTests.java
+++ src/test/java/org/softuni/cardealer/service/SupplierServiceTests.java
@@ -13,12 +13,13 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @DataJpaTest
 @RunWith(SpringRunner.class)
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class SupplierServiceTests {
 
-    private static final String UNMACHED_PARAM = "%s doesn't match!";
+    private SupplierServiceModel testSupplier;
 
     private ModelMapper modelMapper;
     private SupplierService supplierService;
@@ -26,27 +27,122 @@ public class SupplierServiceTests {
     @Autowired
     private SupplierRepository supplierRepository;
 
+    private String unmatchedParamFor(String param) {
+        return String.format("%s doesn't match!", param);
+    }
+
     @Before
     public void init() {
         modelMapper = new ModelMapper();
         supplierService = new SupplierServiceImpl(supplierRepository, modelMapper);
+        testSupplier = new SupplierServiceModel();
+        testSupplier.setName("FirstTestName");
     }
 
     @Test
-    public void SupplierService_SaveCorrectSupplier_ReturnsSameOne() {
-        SupplierServiceModel prep = new SupplierServiceModel();
-        prep.setName("TestName");
-        prep.setImporter(true);
-
-        SupplierServiceModel actual = supplierService.saveSupplier(prep);
+    public void saveSupplier_whenSaveCorrectSupplier_expectSameOne() {
+        SupplierServiceModel actual = supplierService.saveSupplier(testSupplier);
 
         SupplierServiceModel expected = modelMapper
                 .map(supplierRepository.findAll().get(0), SupplierServiceModel.class);
 
-        assertEquals(String.format(UNMACHED_PARAM, "Id"), expected.getId(), actual.getId());
-        assertEquals(String.format(UNMACHED_PARAM, "Name"), expected.getName(), actual.getName());
-        assertEquals(String.format(UNMACHED_PARAM, "Importer"), expected.isImporter(), actual.isImporter());
+        assertEquals(unmatchedParamFor("Id"), expected.getId(), actual.getId());
+        assertEquals(unmatchedParamFor("Name"), expected.getName(), actual.getName());
+        assertEquals(unmatchedParamFor("Importer"), expected.isImporter(), actual.isImporter());
     }
 
+    @Test(expected = Exception.class)
+    public void saveSupplier_whenSaveNullSupplier_expectException(){
+        supplierService.saveSupplier(new SupplierServiceModel());
+    }
 
+    @Test
+    public void editSupplier_whenEditSupplier_expectEditedValues() {
+        SupplierServiceModel supplier = supplierService.saveSupplier(testSupplier);
+
+        String name = "DifferentTestName";
+
+        supplier.setName(name);
+        supplier.setImporter(false);
+
+        SupplierServiceModel expected = supplierService.editSupplier(supplier);
+
+        assertEquals(unmatchedParamFor("Name"), expected.getName(), name);
+        assertFalse(unmatchedParamFor("Importer"), expected.isImporter());
+    }
+
+    @Test
+    public void editSupplier_whenSameSupplier_expectSameValues() {
+        SupplierServiceModel actual = supplierService.saveSupplier(testSupplier);
+
+        SupplierServiceModel expected = supplierService.editSupplier(actual);
+
+        assertEquals(unmatchedParamFor("Name"), expected.getName(), actual.getName());
+        assertEquals(unmatchedParamFor("Importer"), expected.isImporter(), actual.isImporter());
+    }
+
+    @Test(expected = Exception.class)
+    public void editSupplier_whenIdIsNull_expectException() {
+        supplierService.saveSupplier(testSupplier);
+
+        supplierService.editSupplier(testSupplier);
+    }
+
+    @Test(expected = Exception.class)
+    public void editSupplier_whenNameIsNull_expectException() {
+        SupplierServiceModel supplier = supplierService.saveSupplier(testSupplier);
+
+        supplier.setName(null);
+        supplierService.editSupplier(supplier);
+    }
+
+    @Test
+    public void deleteSupplier_whenDeletedSupplier_expectSameSupplier() {
+        SupplierServiceModel actual = supplierService.saveSupplier(testSupplier);
+
+        SupplierServiceModel expected = supplierService.deleteSupplier(actual.getId());
+
+        assertEquals(unmatchedParamFor("Name"), expected.getName(), actual.getName());
+        assertEquals(unmatchedParamFor("Importer"), expected.isImporter(), actual.isImporter());
+    }
+
+    @Test
+    public void deleteSupplier_whenDeletedSupplier_expectNoSupplierInRepository() {
+        SupplierServiceModel supplier = supplierService.saveSupplier(testSupplier);
+
+        supplierService.deleteSupplier(supplier.getId());
+
+        assertNull(supplierRepository.findById(supplier.getId()).orElse(null));
+    }
+
+    @Test(expected = Exception.class)
+    public void deleteSupplier_whenNotExistSupplier_expectException() {
+        SupplierServiceModel firstSupplier = supplierService.saveSupplier(testSupplier);
+        testSupplier.setName("DifferentName");
+        testSupplier.setImporter(false);
+        SupplierServiceModel secondSupplier = supplierService.saveSupplier(testSupplier);
+
+        supplierService.deleteSupplier(secondSupplier.getId());
+        supplierService.deleteSupplier(secondSupplier.getId());
+    }
+
+    @Test(expected = Exception.class)
+    public void deleteSupplier_whenIdIsNull_expectException() {
+        supplierService.deleteSupplier(null);
+    }
+
+    @Test
+    public void findSupplierById_whenFoundSupplier_expectSameSupplier() {
+        SupplierServiceModel actual = supplierService.saveSupplier(testSupplier);
+
+        SupplierServiceModel expected = supplierService.findSupplierById(actual.getId());
+
+        assertEquals(unmatchedParamFor("Name"), expected.getName(), actual.getName());
+        assertEquals(unmatchedParamFor("Importer"), expected.isImporter(), actual.isImporter());
+    }
+
+    @Test(expected = Exception.class)
+    public void findSupplierById_whenIdIsNull_expectException() {
+        supplierService.findSupplierById(null);
+    }
 }
